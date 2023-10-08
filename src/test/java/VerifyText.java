70ef7b4405e569ba91ecde1a21feb9fb05cad647
Geeker2023 <geek2023er@gmail.com>
2023-10-08 21:47:25 +0300

    Verify text

--- src/test/java/VerifyText.java
+++ src/test/java/VerifyText.java
@@ -6,7 +6,7 @@ public class VerifyText {
   @Test
   public void testHelloFrom(){
     Response response = RestAssured
-            .get(path: "https://playground.learnqa.ru/api/get_text")
+            .get("https://playground.learnqa.ru/api/get_text")
             .andReturn();
     response.prettyPrint();
   }
