643a40ba27daf35cd4c0e155784e9d8a0c335b21
Geeker2023 <geek2023er@gmail.com>
2023-10-30 21:29:11 +0300

    Ex 10 Les 3

---
+++ src/test/java/StringLen.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class StringLen {
+  @Test
+  public void stringLen(){
+    String string = "Hello, World!";
+    int actualLength = string.length();
+    assertTrue(actualLength >= 10, "String length is less than 15 characters");
+  }
+}
