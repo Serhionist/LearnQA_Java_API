7ee3a244585e02bd88ea47a017ec11ac64eeaea5
Geeker2023 <geek2023er@gmail.com>
2023-10-30 20:51:28 +0300

    Ex 6

---
+++ src/test/java/Redirect.java
@@ -0,0 +1,23 @@
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class Redirect {
+
+  @Test
+  public void testRestAssured() {
+
+    Response response = RestAssured
+            .given()
+            .redirects()
+            .follow(false)
+            .when()
+            .get("https://playground.learnqa.ru/api/long_redirect")
+            .andReturn();
+
+    response.prettyPrint();
+
+    String locationHeader = response.getHeader("Location");
+    System.out.println(locationHeader);
+  }
+}
