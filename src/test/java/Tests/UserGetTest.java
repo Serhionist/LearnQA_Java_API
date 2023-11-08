ae492b656bbd2f0750c98aa412ec283d940f3e7b
Geeker2023 <geek2023er@gmail.com>
2023-11-08 17:05:25 +0300

    Ex 16 Les 4

--- src/test/java/Tests/UserGetTest.java
+++ src/test/java/Tests/UserGetTest.java
@@ -4,17 +4,28 @@ import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import lib.Assertions;
 import lib.BaseTestCase;
+import lib.DataGenerator;
 import org.junit.jupiter.api.Test;
+import lib.ApiCoreRequests;
 
-import java.util.HashMap;
 import java.util.Map;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.junit.jupiter.api.DisplayName;
+
+@Epic("Get user data depending on permissions cases")
+@Feature("Get user data")
 public class UserGetTest extends BaseTestCase {
+  private final ApiCoreRequests apiCoreRequests = new ApiCoreRequests();
+
   @Test
+  @Description("This test verifies that unauthorized user does not have access to existing users PII")
+  @DisplayName("Receiving a user PII by unauthorized user")
   public void testGetUserDataNotAuth() {
-    Response responseUserData = RestAssured
-            .get("https://playground.learnqa.ru/api/user/2")
-            .andReturn();
+    Response responseUserData = apiCoreRequests
+            .makeGetRequestWoAuthData("https://playground.learnqa.ru/api/user/2");
 
     Assertions.assertJsonHasField(responseUserData, "username");
     Assertions.assertJsonHasNotField(responseUserData, "firstName");
@@ -23,28 +34,43 @@ public class UserGetTest extends BaseTestCase {
   }
 
   @Test
+  @Description("This test verifies that authorized user has access to his own PII")
+  @DisplayName("Receiving own PII data by authorized user")
   public void testGetUserDetailsAuthAsSameUser() {
-    Map<String, String> authData = new HashMap<>();
-    authData.put("email", "vinkotov@example.com");
-    authData.put("password", "1234");
+    Map<String, String> authData = DataGenerator.getRegisteredUserData();
 
-    Response responseGetAuth = RestAssured
-            .given()
-            .body(authData)
-            .post("https://playground.learnqa.ru/api/user/login")
-            .andReturn();
+    Response responseGetAuth = apiCoreRequests
+            .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
 
     String header = this.getHeader(responseGetAuth, "x-csrf-token");
     String cookie = this.getCookie(responseGetAuth, "auth_sid");
 
-    Response responseUserData = RestAssured
-            .given()
-            .header("x-csrf-token", header)
-            .cookie("auth_sid", cookie)
-            .get("https://playground.learnqa.ru/api/user/2")
-            .andReturn();
+    Response responseUserData = apiCoreRequests
+            .makeGetRequest("https://playground.learnqa.ru/api/user/2", header, cookie);
 
     String[] expectedFields = {"username", "firstName", "lastName", "email"};
     Assertions.assertJsonHasFields(responseUserData, expectedFields);
+  }
+
+  @Test
+  @Description("This test verifies that authorized user does not have access to other users PII")
+  @DisplayName("Receiving othe user PII data by authorized user")
+  public void testGetUserDetailsAuthAsDifferentUser() {
+    Map<String, String> authData = DataGenerator.getRegisteredUserData();
+
+    Response responseGetAuth = apiCoreRequests
+            .makePostRequest("https://playground.learnqa.ru/api/user/login", authData);
+
+    String header = this.getHeader(responseGetAuth, "x-csrf-token");
+    String cookie = this.getCookie(responseGetAuth, "auth_sid");
+
+    Response responseUserData = apiCoreRequests
+            .makeGetRequest("https://playground.learnqa.ru/api/user/1", header, cookie);
+
+    String[] expectedFields = {"username", "firstName", "lastName", "email"};
+    Assertions.assertJsonHasField(responseUserData, "username");
+    Assertions.assertJsonHasNotField(responseUserData, "firstName");
+    Assertions.assertJsonHasNotField(responseUserData, "lastName");
+    Assertions.assertJsonHasNotField(responseUserData, "email");
   }
 }
